8d5e035897c0865cc1274b6b494cbf9efb022750
Philip Mark Deazeta <pmdeazeta@gmail.com>
2012-02-28 15:31:19 +0800

    DOCA-14 -added dynamic finders -added queries for public and private images -added function to show image metadata

--- lib/fog/openstack/models/image/images.rb
+++ lib/fog/openstack/models/image/images.rb
@@ -16,15 +16,39 @@ module Fog
         end
 
         def find_by_id(id)
-          self.find {|image| image.id == id} ||
-            Fog::Image::OpenStack::Image.new(
-              connection.get_image(id).body['image'])
+          self.find {|image| image.id == id}
+        end
+
+        def public
+          images = load(connection.list_public_images_detailed.body['images'])
+          images.delete_if{|image| image.is_public == false}
+        end
+
+        def private
+          images = load(connection.list_public_images_detailed.body['images'])
+          images.delete_if{|image| image.is_public}
         end
 
         def destroy(id)
           image = self.find_by_id(id)
           image.destroy
         end
+
+#        queries available through image service
+#        find_by_container_format=FORMAT
+#        find_by_disk_format=FORMAT
+#        find_by_status=STATUS
+#        find_by_size_min=BYTES
+#        find_by_size_max=BYTES
+
+        def method_missing(method_sym, *arguments, &block)
+          if method_sym.to_s =~ /^find_by_(.*)$/
+            load(connection.list_public_images_detailed($1 ,arguments.first).body['images'])
+          else
+            super
+          end
+        end
+
       end
     end
   end
